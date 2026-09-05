be1562698cbb102c4acb2529b28ade779d5c05c9
Xkyliver <xkyliv@gmail.com>
2026-09-05 22:46:01 +0530

    Increase loop range from 20 to 300

--- utils.sh
+++ utils.sh
@@ -492,7 +492,7 @@ dl_uptodown() {
 	data_code=$($HTMLQ "#detail-app-name" --attribute data-code <<<"$__UPTODOWN_RESP__")
 	local versionURL=""
 	local is_bundle=false
-	for i in {1..20}; do
+	for i in {1..300}; do
 		resp=$(req "${uptodown_dlurl}/apps/${data_code}/versions/${i}" -)
 		if ! op=$(jq -e -r ".data | map(select(.version == \"${version}\")) | .[0]" <<<"$resp"); then
 			continue
